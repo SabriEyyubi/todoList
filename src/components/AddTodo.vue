48379d3763a1336e9c1645a482e4eda140669c1d
SabriEyyubi <sabrieyyubi@gmail.com>
2021-01-27 13:09:48 +0300

    todoCommit

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+        <h3>Add Todo</h3>
+        <div class="add">
+            <form @submit = "onSubmit">
+                <input type="text" v-model = "title" placeholder="Add Todo...">
+                <input type="submit" value = "Submit">
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+import {mapActions} from 'vuex';
+export default {
+    name: "AddTodo",
+    data(){
+        return{
+            title : '' //v-model
+        }
+    },
+    methods:{
+        ...mapActions(['AddTodo']),
+        onSubmit(e){
+            e.preventDefault();
+            this.AddTodo(this.title);
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './styles/scssstyle.scss';
+
+@mixin color {
+    background: gray;
+    color: #fff;
+    border: 1px red solid;
+    
+} 
+
+
+form{
+    display: flex;
+    
+
+    input[type="text"]{
+
+    flex: 10;
+    padding: 10px;
+    background: lightblue;
+    outline: 0;
+}
+input[type="submit"]{
+    flex:2;
+    @include color();
+    @include test();
+    
+    cursor: pointer;
+}
+}
+
+
+
+
+
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+    <h3>Todos</h3>
+    <div class="todos">
+        <div v-for="todo in allTodos" :key="todo.id" class="todo">
+            {{todo.title}}
+            <i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
+        </div>
+    </div>
+    
+  </div>
+    </template>
+<script>
+   import {mapActions, mapGetters} from 'vuex';
+
+    export default{
+        name: "Todos",
+        methods:{
+            ...mapActions(['fetchTodos',"deleteTodo"]),
+        },
+        computed: mapGetters(['allTodos']),
+        created(){
+            this.fetchTodos();
+        }
+    };
+</script>
+
+<style lang= "scss" scoped>
+
+    
+
+    @mixin display($color){
+        text-align: center;
+        position: relative;
+        background: $color;
+    }
+    div{
+        h3{
+            height: 50px;
+            width: 50px;
+             @include display( #6f41b8);
+            
+        }
+.todos{
+        display: grid;
+        grid-template-columns: repeat(4,1fr);
+        grid-gap: 1rem;
+    }
+    .todo{
+        @include display( #6f41b8);
+        border: 1px solid #ccc;       
+        padding: 1rem;
+        border-radius: 3px;       
+        cursor: pointer;
+    }
+    .fas fa-trash-alt{
+        
+    
+      position: absolute;
+      bottom: 10px;
+      right: 10px;
+      color: black;
+      cursor: pointer;  
+    }
+    }
+    
+
+</style>
